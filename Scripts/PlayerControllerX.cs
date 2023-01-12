a1a4882e4068cad98e45ac905263c7764005c3bc
Tommy <tommyboy936@gmail.com>
2023-01-11 19:22:56 -0800

    Game_Fix
    WAs tasked with fixing some of the code in this game.

---
+++ Scripts/PlayerControllerX.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+
+    private float speed = 500;
+   
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+
+    private float normalStrength = 10; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+    private float speedBoost = 100000;
+
+    Coroutine powerUpTimer;
+
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+       
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * Time.deltaTime); 
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(focalPoint.transform.forward * speedBoost * Time.deltaTime);
+            Debug.Log("That boy is going fast");
+            
+        }
+         
+    }
+
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerUpTimer = StartCoroutine(PowerupCooldown(7));
+            powerupIndicator.SetActive(true);
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown(int countLimit)
+    {
+        int i = 0;
+        while (i < countLimit)
+        {
+            i++;
+            Debug.Log(i);
+            yield return new WaitForSeconds(1);
+        }
+        Debug.Log("Finished");
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position; 
+           
+            if (hasPowerup == true) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+
+
+        }
+    }
+
+
+
+}
---
+++ Scripts/SpawnManagerX.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
+    private float spawnRangeX = 10;
+    private float spawnZMin = 15; // set min spawn Z
+    private float spawnZMax = 25; // set max spawn Z
+
+    public int enemyCount;
+    public int waveCount = 1;
+
+
+    public GameObject player;
+
+    private EnemyX enemySpeedscript;
+
+    private void Start()
+    {
+        enemySpeedscript = GameObject.Find("Enemy").GetComponent<EnemyX>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<EnemyX>().Length;
+
+        if (enemyCount == 0)
+        {
+            waveCount++;
+            SpawnEnemyWave(waveCount);
+            PowerUpSpawn();
+            ResetPlayerPosition(); // put player back at start
+            enemySpeedscript.speed++;
+            Debug.Log(enemySpeedscript.speed);
+        }
+
+    }
+
+    // Generate random spawn position for powerups and enemy balls
+    private Vector3 GenerateSpawnPosition ()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        Vector3 randomPos = new Vector3(xPos, 0, zPos);
+        return randomPos;
+    }
+
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        // Spawn number of enemy balls based on wave number
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    // Move player back to position in front of own goal
+    void ResetPlayerPosition ()
+    {
+        player.transform.position = new Vector3(0, 1, -7);
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+    }
+    void PowerUpSpawn()
+    {
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+}
